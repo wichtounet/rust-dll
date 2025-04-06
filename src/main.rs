1d71a560d8cde950117c3bf48fe2e3072465ab60
Baptiste Wicht <baptiste.wicht@gmail.com>
2025-04-06 08:23:32 +0200

    Use the etl crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use etl::vector::Vector;
+
+fn read_mnist_images(train: bool) -> Vec<Vector<i32>> {
+    Vec::new()
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let train_images = read_mnist_images(true);
 }
