a0f76a012ddba9da5f88eb7f67cbef0f27ee0e71
Baptiste Wicht <baptiste.wicht@gmail.com>
2025-04-12 10:22:10 +0200

    Extract mnist mod

---
+++ src/mnist.rs
@@ -0,0 +1,63 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use etl::etl_expr::EtlExpr;
+use etl::matrix_2d::Matrix2d;
+use etl::vector::Vector;
+
+fn read_header(data: &[u8], pos: usize) -> u32 {
+    ((data[pos] as u32) << 24) + ((data[pos + 1] as u32) << 16) + ((data[pos + 2] as u32) << 8) + (data[pos + 3] as u32)
+}
+
+pub fn read_mnist_images_1d(train: bool) -> Vec<Vector<f32>> {
+    let mut data: Vec<u8> = Vec::new();
+
+    let path = if train { "datasets/train-images-idx3-ubyte" } else { "datasets/t10k-images-idx3-ubyte" };
+
+    let mut f = File::open(path).expect("File not found");
+    f.read_to_end(&mut data).expect("Cannot read the file");
+
+    let magic = read_header(&data, 0);
+
+    if magic != 0x803 {
+        panic!("Invalid magic number in MNIST file");
+    }
+
+    let count = read_header(&data, 4) as usize;
+    let rows = read_header(&data, 8) as usize;
+    let columns = read_header(&data, 12) as usize;
+
+    let mut images = Vec::<Vector<f32>>::new();
+
+    for i in 0..count {
+        let mut image = Vector::<f32>::new((rows * columns) as usize);
+
+        for c in 0..(rows * columns) as usize {
+            image[c] = data[12 + i * rows * columns + c] as f32;
+        }
+
+        images.push(image);
+    }
+
+    images
+}
+
+pub fn images_1d_to_batches(images: &Vec<Vector<f32>>, batch_size: usize) -> Vec<Matrix2d<f32>> {
+    let mut batches = Vec::<Matrix2d<f32>>::new();
+
+    let end = (images.len() / batch_size) * batch_size;
+
+    for b in (0..end).step_by(batch_size) {
+        let mut batch = Matrix2d::<f32>::new(batch_size, images[b].size());
+
+        for i in 0..batch_size {
+            for p in 0..images[b + i].size() {
+                *batch.at_mut(i, p) = images[b + i].at(p);
+            }
+        }
+
+        batches.push(batch);
+    }
+
+    batches
+}
