b9fe2d44a77bd738bb90516244bba54bc442ea48
Baptiste Wicht <baptiste.wicht@gmail.com>
2025-05-04 13:59:26 +0200

    Cleanup main function

--- src/main.rs
+++ src/main.rs
@@ -16,44 +16,17 @@ fn main() {
     let mut train_images = read_mnist_images_1d(true);
     let mut test_images = read_mnist_images_1d(false);
 
-    println!("Train images: {}", train_images.len());
-    println!("Test images: {}", test_images.len());
-
     normalize_images_1d(&mut train_images);
     normalize_images_1d(&mut test_images);
-
-    let train_labels = read_mnist_labels(true);
-    let test_labels = read_mnist_labels(false);
-
-    println!("Train labels: {}", train_labels.len());
-    println!("Test labels: {}", test_labels.len());
 
     let train_cat_labels = read_mnist_categorical_labels(true);
     let test_cat_labels = read_mnist_categorical_labels(false);
 
-    println!("Cat. Train labels: {}", train_cat_labels.len());
-    println!("Cat. Test labels: {}", test_cat_labels.len());
-
     let test_batches = images_1d_to_batches(&test_images, batch_size);
     let train_batches = images_1d_to_batches(&train_images, batch_size);
 
-    println!("Train batches: {}", train_batches.len());
-    println!("Test batches: {}", test_batches.len());
-
-    let test_label_batches = labels_to_batches(&test_labels, batch_size);
-    let train_label_batches = labels_to_batches(&train_labels, batch_size);
-
-    println!("Train label batches: {}", train_label_batches.len());
-    println!("Test label batches: {}", test_label_batches.len());
-
     let test_cat_label_batches = categorical_labels_to_batches(&test_cat_labels, batch_size);
     let train_cat_label_batches = categorical_labels_to_batches(&train_cat_labels, batch_size);
-
-    println!("Cat. Train label batches: {}", train_cat_label_batches.len());
-    println!("Cat. Test label batches: {}", test_cat_label_batches.len());
-
-    println!("Test label 0: {}", test_labels.first().expect("No labels"));
-    println!("Test label 0: {}", test_cat_labels.first().expect("No labels"));
 
     let mut mlp = Network::new();
     mlp.add_layer(Box::new(DenseLayer::new_sigmoid(28 * 28, 500)));
